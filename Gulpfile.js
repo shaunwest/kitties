8e13ed00bbbd635f24e1fe027be21e616395d1bf
Shaun West <shaunwest@gmail.com>
2014-12-28 21:35:43 -0500

    initial commit

---
+++ Gulpfile.js
@@ -0,0 +1,64 @@
+/**
+ * Created by Shaun on 11/13/2014.
+ */
+
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var karma = require('karma').server;
+var runSequence = require('run-sequence');
+var clean = require('gulp-clean');
+var karmaConfig = __dirname + '/karma.conf.js';
+var jsSources = [
+  'bower_components/kilo-core/src/kilo-core.js',
+  'bower_components/kilo-extra/src/**/*.js',
+  'bower_components/kilo-scheduler/src/**/*.js',
+  'bower_components/kilo-flow/src/**/*.js',
+  'src/**/*.js'
+];
+
+gulp.task('clean', function() {
+  return gulp.src('dist', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('build', function() {
+  return gulp.src(jsSources)
+    .pipe(concat('kitties.js'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build-prod', function() {
+  return gulp.src(jsSources)
+    .pipe(concat('kitties.js'))
+    .pipe(gulp.dest('dist'))
+    .pipe(uglify())
+    .pipe(rename('kitties.min.js'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('test', function(cb) {
+  return karma.start({
+    configFile: karmaConfig,
+    singleRun: true
+  }, cb);
+});
+
+gulp.task('watch', function() {
+  return gulp.watch('src/**/*.js', ['build']);
+});
+
+gulp.task('ci', function(cb) {
+  return karma.start({
+    configFile: karmaConfig
+  }, cb);
+});
+
+gulp.task('default', function(cb) {
+  runSequence('clean', 'build', 'watch', cb);
+});
+
+gulp.task('prod', function(cb) {
+  runSequence('test', 'clean', 'prod-build', cb);
+});
